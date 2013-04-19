c61dc16731426f448eb27be5fd87ba5a5990eff8
Christopher Wellons <mosquitopsu@gmail.com>
2013-04-19 16:58:40 -0400

    Drop defer.

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-var display = new Display(document.getElementById('display').getContext('2d'));
-var game = new Snake(display);
-var controls = new Controls(game);
-game.run();
+window.addEventListener('load', function() {
+    var context = document.getElementById('display').getContext('2d');
+    var display = new Display(context);
+    var game = new Snake(display);
+    var controls = new Controls(game);
+    game.run();
+});
