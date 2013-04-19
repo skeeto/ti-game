e56fbd5247f77a88b7dfda9d80136518ea708ce0
Christopher Wellons <mosquitopsu@gmail.com>
2013-04-19 18:03:14 -0400

    Add an image displayer "game".

---
+++ ImageGame.js
@@ -0,0 +1,36 @@
+function ImageGame(display, url) {
+    Game.call(this, display);
+    this.framerate = 10;
+    this.invert = true;
+    this.data = null;
+
+    var img = new Image();
+    img.src = url;
+    img.onload = function() {
+        var width = Display.WIDTH, height = Display.HEIGHT;
+        var context = document.createElement('canvas').getContext('2d');
+        context.canvas.width = width;
+        context.canvas.height = height;
+        context.drawImage(img, 0, 0);
+        this.data = context.getImageData(0, 0, width, height).data;
+    }.bind(this);
+}
+
+ImageGame.prototype = Object.create(Game.prototype);
+ImageGame.prototype.constructor = ImageGame;
+
+ImageGame.prototype.step = function() {
+    var data = this.data;
+    if (data != null) {
+        var width = Display.WIDTH, height = Display.HEIGHT;
+        for(var i = 0; i < data.length; i += 4) {
+            var r = data[i],
+                g = data[i+1],
+                b = data[i+2];
+            var brightness = (3 * r + 4 * g + b) >>> 3;
+            if (this.invert) brightness = 255 - brightness;
+            var point = Point((i / 4) % width, ~~((i / 4) / width));
+            this.display.set(point, ~~((brightness / 255) * 4));
+        }
+    }
+};
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function() {
     var context = document.getElementById('display').getContext('2d');
     var display = new Display(context);
     //var game = new Snake(display);
-    var game = new VideoGame(display, 'opencl.webm');
+    var game = new ImageGame(display, 'Snake.png');
     var controls = new Controls(game);
     game.run();
 });
