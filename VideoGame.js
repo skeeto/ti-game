f644a30bd5d6d55eed17b54e5db8ce0403e41846
Christopher Wellons <mosquitopsu@gmail.com>
2013-04-19 17:21:06 -0400

    Add a video player.

---
+++ VideoGame.js
@@ -0,0 +1,39 @@
+function VideoGame(display, url) {
+    Game.call(this, display);
+    this.video = document.createElement('video');
+    this.video.src = url;
+    this.video.loop = true;
+    this.background = document.createElement('canvas').getContext('2d');
+    this.background.canvas.width = Display.WIDTH;
+    this.background.canvas.height = Display.HEIGHT;
+}
+
+VideoGame.prototype = Object.create(Game.prototype);
+VideoGame.prototype.constructor = VideoGame;
+
+VideoGame.prototype.run = function() {
+    if (Game.prototype.run.call(this)) {
+        this.video.play();
+        console.log('play');
+    }
+};
+
+VideoGame.prototype.stop = function() {
+    if (Game.prototype.stop.call(this)) {
+        this.video.stop();
+    }
+};
+
+VideoGame.prototype.step = function() {
+    var width = Display.WIDTH, height = Display.HEIGHT;
+    this.background.drawImage(this.video, 0, 0, width, height);
+    var data = this.background.getImageData(0, 0, width, height).data;
+    for(var i = 0; i < data.length; i += 4) {
+        var r = data[i],
+            g = data[i+1],
+            b = data[i+2];
+        var brightness = (3 * r + 4 * g + b) >>> 3;
+        var point = Point((i / 4) % width, ~~((i / 4) / width));
+        this.display.set(point, ~~((brightness / 255) * 4));
+    }
+};
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 window.addEventListener('load', function() {
     var context = document.getElementById('display').getContext('2d');
     var display = new Display(context);
-    var game = new Snake(display);
+    //var game = new Snake(display);
+    var game = new VideoGame(display, 'opencl.webm');
     var controls = new Controls(game);
     game.run();
 });
