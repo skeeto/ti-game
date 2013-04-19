db3bcd017d7ea2c0d890c13d8ec7b5945642e0f4
Christopher Wellons <mosquitopsu@gmail.com>
2013-04-19 19:23:03 -0400

    Make the title screen work.

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 window.addEventListener('load', function() {
     var context = document.getElementById('display').getContext('2d');
     var display = new Display(context);
-    //var game = new Snake(display);
     var video = new VideoGame(display, 'opencl.webm');
     var image = new ImageGame(display, 'Snake.png');
-    var game = new ComposeGame(video, image);
-    var controls = new Controls(game);
-    game.run();
+    var title = new ComposeGame(video, image);
+    var controls = new Controls(title);
+    title.run();
+    title.press = function() {
+        title.stop();
+        controls.remove(title);
+        display.clear();
+        var snake = new Snake(display);
+        controls.add(snake);
+        snake.run();
+    };
 });
