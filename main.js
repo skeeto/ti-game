34ff12b06150e7812ff0c6f26ee5791cb765b479
Christopher Wellons <mosquitopsu@gmail.com>
2013-04-19 20:25:21 -0400

    Pick between two title screens at random.

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 window.addEventListener('load', function() {
+    var videos = ['pycube.webm', 'opencl.webm'];
     var context = document.getElementById('display').getContext('2d');
     var display = new Display(context);
-    var video = new VideoGame(display, 'opencl.webm');
+    var video = new VideoGame(display, videos.random());
     var image = new ImageGame(display, 'Snake.png');
     var title = new ComposeGame(video, image);
     title.press = function() {
--- util.js
+++ util.js
@@ -1,3 +1,7 @@
 function rand(n) {
     return ~~(Math.random() * n);
 }
+
+Array.prototype.random = function() {
+    return this[~~(this.length * Math.random())];
+};
